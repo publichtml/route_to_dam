d80dd18375a41c014c4522761fd787d422cf769c
publichtml <publi9html@gmail.com>
2016-07-11 01:07:03 +0900

    Add routes for dam resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :dams
+
+  # 以下8行は "resources :dams" と等価
+  # get    'dams',          to: 'dams#index'
+  # post   'dams',          to: 'dams#create'
+  # get    'dams/new',      to: 'dams#new',   as: :new_dam
+  # get    'dams/:id/edit', to: 'dams#edit',  as: :edit_dam
+  # get    'dams/:id',      to: 'dams#show',  as: :dam
+  # patch  'dams/:id',      to: 'dams#update'
+  # put    'dams/:id',      to: 'dams#update' # deprecated
+  # delete 'dams/:id',      to: 'dams#destroy'
 end
