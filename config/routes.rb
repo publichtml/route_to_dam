aaaa959bea2bc0261ec0b983217b1e31a2f4e7be
publichtml <publi9html@gmail.com>
2016-07-11 02:12:53 +0900

    Fix indents

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
   namespace :admin do
-  resources :dams do
-    resource  :reservoir
-    resources :discharge_facilities
-    patch     :water_level
-  end
+    resources :dams do
+      resource  :reservoir
+      resources :discharge_facilities
+      patch     :water_level
+    end
 
-  # 以下8行は "resources :dams" と等価
-  # get    'dams',          to: 'dams#index'
-  # post   'dams',          to: 'dams#create'
-  # get    'dams/new',      to: 'dams#new',   as: :new_dam
-  # get    'dams/:id/edit', to: 'dams#edit',  as: :edit_dam
-  # get    'dams/:id',      to: 'dams#show',  as: :dam
-  # patch  'dams/:id',      to: 'dams#update'
-  # put    'dams/:id',      to: 'dams#update' # deprecated
-  # delete 'dams/:id',      to: 'dams#destroy'
+    # 以下8行は "resources :dams" と等価
+    # get    'dams',          to: 'dams#index'
+    # post   'dams',          to: 'dams#create'
+    # get    'dams/new',      to: 'dams#new',   as: :new_dam
+    # get    'dams/:id/edit', to: 'dams#edit',  as: :edit_dam
+    # get    'dams/:id',      to: 'dams#show',  as: :dam
+    # patch  'dams/:id',      to: 'dams#update'
+    # put    'dams/:id',      to: 'dams#update' # deprecated
+    # delete 'dams/:id',      to: 'dams#destroy'
   end
 end
